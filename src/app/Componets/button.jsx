e4958a6a07d859544b70ed2d030a2c3f49dafeff
Guilherme <147566218+Guilherm001@users.noreply.github.com>
2024-12-20 13:41:02 +0000

    testando o bootstrap

---
+++ src/app/Componets/button.jsx
@@ -0,0 +1,14 @@
+export default function buttor(){
+    return(
+        <div className="card ">
+            <div className="card-body">
+                <h5 className="card-title">Título do card</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
+                <p className="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                <a href="#" className="card-link">Link do card</a>
+                <a href="#" className="card-link">Outro link</a>
+            </div>
+        </div>
+
+    )
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+
+//**componentes */
+import Botao from './Componets/button'
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main></main>
+      <main>
+        
+        <Botao />
+    
+      </main>
     </div>
   );
 }
